431ee6337b386e73004995e5d11bd0909714e49d
Danik <89976419+ByanonymPerson@users.noreply.github.com>
2024-01-30 20:36:18 +0300

    productlist and form add

---
+++ src/components/Productlist/Productlist.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Productlist.css'
+
+const ProductList = () => {
+    return(
+        <div>
+            ProductList
+        </div>
+    )
+}
+
+export default ProductList;
