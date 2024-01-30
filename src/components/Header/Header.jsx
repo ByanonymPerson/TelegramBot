6c53147266914a6fe5a1594abbe0a988209ab72e
Danik <89976419+ByanonymPerson@users.noreply.github.com>
2024-01-30 20:25:48 +0300

    bug fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
+import './Header.css';
 
+const Header = () => {
+    const { tg, user, onClose } = useTelegram(); // Добавлено tg
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {tg.initDataUnsafe?.user?.username} {/* Исправлено на tg.initDataUnsafe?.user?.username */}
+            </span>
+        </div>
+    );
+}
 
-const Header = () =>{
-      const { user , onClose} = useTelegram();
- return (
-    <div className={'header'}>
-        <Button onClick={onClose}>Закрыть</Button>
-        <span className={'username'}>
-            {tg.initDataUnsafe?.user?.username}
-        </span>
-    </div>
- )
- }
-
- export default Header;
+export default Header;
