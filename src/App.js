80a216e5e499b340ca2b67dc6b88f6c688defb55
Danik <89976419+ByanonymPerson@users.noreply.github.com>
2024-01-30 20:14:18 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import './App.css';
 const tg = window.Telegram.WebApp;
 
 function App() {
-      
+  const {tg, user , onClose , onToggleButton} = useTelegram();
+
       useEffect(() => {
       tg.ready();
       }, [])
 
-
-
   return (
     <div className="App">
-
+     <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
