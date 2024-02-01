ac96651cec39b52845895a6c876727f4a8901abc
Danik <89976419+ByanonymPerson@users.noreply.github.com>
2024-02-01 19:21:14 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useEffect, useState } from "react";
-import './Form.css'
+import './Form.css';
 import { useTelegram } from "../../hooks/useTelegram";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
 
- const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAx7hGnf2H2od3EfujXlmbe4mxYwBwr-BQ",
     authDomain: "telegramstore-96e1e.firebaseapp.com",
     databaseURL: "https://telegramstore-96e1e-default-rtdb.firebaseio.com",
@@ -14,88 +14,80 @@ import { getDatabase, ref, set } from "firebase/database";
     appId: "1:454886096625:web:8fb97c3a405772d44f7350",
     measurementId: "G-S47G5VXH3Z"
   };
-
-  const firebaseApp = initializeApp(firebaseConfig);
-  const database = getDatabase(firebaseApp);
   
 
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+
 const Form = () => {
-     const [name,setName] = useState('');
-     const [time,setTime] = useState('');
-     const {tg} = useTelegram();
-     
-     const onSendData = useCallback(()=>{
-         const data = {
+    const [name, setName] = useState('');
+    const [time, setTime] = useState('');
+    const { tg } = useTelegram();
+
+    const onChangeName = (e) => {
+        setName(e.target.value);
+    }
+
+    const onChangeTime = (e) => {
+        setTime(e.target.value);
+    }
+
+    const sendDataToFirebase = useCallback(() => {
+        const data = {
             name,
             time,
-         }
-         tg.sendData(JSON.stringify(data));
-     }, [name,time])
+        };
 
-     useEffect(()=>{
-        tg.onEvent('mainButtonClicked',onSendData)
-        return() => {
-            tg.offEvent('mainButtonClicked',onSendData)
-        }
-     },[])
-
-     useEffect(() => {
-       tg.MainButton.setParams({
-        text: 'Отправить данные'
-       })
-     }, [])
-
-     useEffect(() => {
-           if(!time || !name){
-            tg.MainButton.hide();
-           }else {
-            tg.MainButton.show();
-           }
-     },[name,time])
-
-     useEffect(() => {
-        sendDataToFirebase();
-    }, [name, time]);
-
-     set(ref(database, 'orders/' + Date.now()), data)
+        set(ref(database, 'orders/' + Date.now()), data)
             .then(() => {
                 console.log('Data successfully sent to Firebase');
             })
             .catch((error) => {
                 console.error('Error sending data to Firebase: ', error);
             });
-    
+    }, [name, time, database]);
 
-     const onChangeName = (e) => {
-        setName(e.target.value)
-     }
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', sendDataToFirebase);
+        return () => {
+            tg.offEvent('mainButtonClicked', sendDataToFirebase);
+        }
+    }, [tg, sendDataToFirebase]);
 
-     const onChangeTime = (e) => {
-        setTime(e.target.value)
-     }
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    }, [tg]);
 
-   
+    useEffect(() => {
+        if (!time || !name) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [tg, name, time]);
 
-    return(
+    return (
         <div className={"form"}>
             <h3>Введите ваши данные</h3>
             <input
-             className={'input'}
-              type="text" 
-              placeholder={'Имя'}
-              value={name}
-              onChange={onChangeName}
-              />
-              <h3>Введите время в которое вам удобно забрать</h3>
-            <input 
-            className={'input'}
-             type="text" 
-             laceholder={'Время'}
-             value={time}
-             onChange={onChangeTime}
-             />
+                className={'input'}
+                type="text"
+                placeholder={'Имя'}
+                value={name}
+                onChange={onChangeName}
+            />
+            <h3>Введите время в которое вам удобно забрать</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Время'}
+                value={time}
+                onChange={onChangeTime}
+            />
         </div>
-    )
+    );
 }
 
 export default Form;
