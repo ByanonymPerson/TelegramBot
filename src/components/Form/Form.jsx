4532afd9c63456613ffdb375de7df6ed377fb64c
Danik <89976419+ByanonymPerson@users.noreply.github.com>
2024-02-01 19:54:44 +0300

    update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,92 +1,71 @@
 import React, { useCallback, useEffect, useState } from "react";
-import './Form.css';
+import './Form.css'
 import { useTelegram } from "../../hooks/useTelegram";
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAx7hGnf2H2od3EfujXlmbe4mxYwBwr-BQ",
-    authDomain: "telegramstore-96e1e.firebaseapp.com",
-    databaseURL: "https://telegramstore-96e1e-default-rtdb.firebaseio.com",
-    projectId: "telegramstore-96e1e",
-    storageBucket: "telegramstore-96e1e.appspot.com",
-    messagingSenderId: "454886096625",
-    appId: "1:454886096625:web:8fb97c3a405772d44f7350",
-    measurementId: "G-S47G5VXH3Z"
-  };
-
-const firebaseApp = initializeApp(firebaseConfig);
-const database = getDatabase(firebaseApp);
 
 const Form = () => {
-    const [name, setName] = useState('');
-    const [time, setTime] = useState('');
-    const { tg } = useTelegram();
-
-    const onChangeName = (e) => {
-        setName(e.target.value);
-    }
-
-    const onChangeTime = (e) => {
-        setTime(e.target.value);
-    }
-
-    const sendDataToFirebase = useCallback(() => {
-        const data = {
+     const [name,setName] = useState('');
+     const [time,setTime] = useState('');
+     const {tg} = useTelegram();
+     
+     const onSendData = useCallback(()=>{
+         const data = {
             name,
             time,
-        };
+         }
+         tg.sendData(JSON.stringify(data));
+     }, [name,time])
 
-        set(ref(database, 'orders/' + Date.now()), data)
-            .then(() => {
-                console.log('Data successfully sent to Firebase');
-            })
-            .catch((error) => {
-                console.error('Error sending data to Firebase: ', error);
-            });
-    }, [name, time, database]);
-
-    useEffect(() => {
-        tg.onEvent('mainButtonClicked', sendDataToFirebase);
-        return () => {
-            tg.offEvent('mainButtonClicked', sendDataToFirebase);
+     useEffect(()=>{
+        tg.onEvent('mainButtonClicked',onSendData)
+        return() => {
+            tg.WebApp.offEvent('mainButtonClicked',onSendData)
         }
-    }, [tg, sendDataToFirebase]);
+     },[])
 
-    useEffect(() => {
-        tg.MainButton.setParams({
-            text: 'Отправить данные'
-        });
-    }, [tg]);
+     useEffect(() => {
+       tg.MainButton.setParams({
+        text: 'Отправить данные'
+       })
+     }, [])
 
-    useEffect(() => {
-        if (!time || !name) {
+     useEffect(() => {
+           if(!time || !name){
             tg.MainButton.hide();
-        } else {
+           }else {
             tg.MainButton.show();
-        }
-    }, [tg, name, time]);
+           }
+     },[name,time])
 
-    return (
+     const onChangeName = (e) => {
+        setName(e.target.value)
+     }
+
+     const onChangeTime = (e) => {
+        setTime(e.target.value)
+     }
+
+   
+
+    return(
         <div className={"form"}>
             <h3>Введите ваши данные</h3>
             <input
-                className={'input'}
-                type="text"
-                placeholder={'Имя'}
-                value={name}
-                onChange={onChangeName}
-            />
-            <h3>Введите время в которое вам удобно забрать</h3>
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Время'}
-                value={time}
-                onChange={onChangeTime}
-            />
+             className={'input'}
+              type="text" 
+              placeholder={'Имя'}
+              value={name}
+              onChange={onChangeName}
+              />
+              <h3>Введите время в которое вам удобно забрать</h3>
+            <input 
+            className={'input'}
+             type="text" 
+             laceholder={'Время'}
+             value={time}
+             onChange={onChangeTime}
+             />
         </div>
-    );
+    )
 }
 
 export default Form;
--- src/components/Productlist/Productlist.jsx
+++ src/components/Productlist/Productlist.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import './Productlist.css'
 import ProductItem from "../ProductItem/ProductItem";
+import { useTelegram } from "../../hooks/useTelegram";
+
+
+const products = [
+    {id: '1' , title: 'Angry vape' , price:15 , description: 'Арбузная жвачка'},
+    {id: '2' , title: 'Angry vape' , price:15 , description: 'Арбуз'},
+    {id: '3' , title: 'Angry vape' , price:15 , description: 'Йогурт с киви'},
+    {id: '4' , title: 'Angry vape' , price:15 , description: 'Синия малина'},
+    {id: '5' , title: 'Angry vape' , price:15 , description: 'Ягодные леденцы'},
+    {id: '6' , title: 'Angry vape' , price:15 , description: 'Клубничный джем'},
+    {id: '7' , title: 'Angry vape' , price:15 , description: 'Вишня лимон'},
+    {id: '8' , title: 'Angry vape' , price:15 , description: 'Виноградная газировка'},
+    {id: '9' , title: 'TOYZ' , price:15 , description: 'Orange,Lemon & Mint'},
+    {id: '10' , title: 'TOYZ' , price:15 , description: 'Orange ice'},
+]
+
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc,item) => {
+        return acc += item.price
+    }, 0)
+}
 
 const ProductList = () => {
+    const [addedItems , setAddedItems] = useState([]);
+    const {tg} = useTelegram();
+    const onAdd =(product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product);
+        } else {
+            newItems = [...addedItems,product];
+        }
+
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
+        }else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
     return(
         <div className={list}>
             {products.map(item=> (
