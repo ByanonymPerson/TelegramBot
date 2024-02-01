df39130cd5ec8c0be6b2c48d17df9732efeabee5
Danik <89976419+ByanonymPerson@users.noreply.github.com>
2024-02-01 20:41:34 +0300

    Update ProductItem.jsx

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -8,18 +8,20 @@ const ProductItem = ({ product, className, onAdd }) => {
     };
 
     return (
-        <div className={'product ' + className}> 
-            <div className={'img'}/>
+        <div className={'product ' + className}>
+            <div className={'img'}>
+                <img src={product.img} alt={product.title} /> 
+            </div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
-                 <span>Cтоимость: <b>{product.price}</b></span> 
+                <span>Cтоимость: <b>{product.price}</b></span>
             </div>
-             <Button className={'add-btn'} onClick={onAddHandler}> 
-                 Добавить в корзину
-             </Button>
+            <Button className={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
+            </Button>
         </div>
     );
-}
+};
 
 export default ProductItem; // Исправлено на ProductItem
