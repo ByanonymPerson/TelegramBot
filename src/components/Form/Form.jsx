2807b54750d4f745ce57e3f84b141471a3ff34cc
Danik <89976419+ByanonymPerson@users.noreply.github.com>
2024-02-01 18:05:02 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import './Form.css'
 import { useTelegram } from "../../hooks/useTelegram";
 
@@ -6,6 +6,21 @@ const Form = () => {
      const [name,setName] = useState('');
      const [time,setTime] = useState('');
      const {tg} = useTelegram();
+     
+     const onSendData = useCallback(()=>{
+         const data = {
+            name,
+            time,
+         }
+         tg.sendData(JSON.stringify(data));
+     }, [])
+
+     useEffect(()=>{
+        tg.onEvent('mainButtonClicked',onSendData)
+        return() => {
+            tg.offEvent('mainButtonClicked',onSendData)
+        }
+     },[])
 
      useEffect(() => {
        tg.MainButton.setParams({
