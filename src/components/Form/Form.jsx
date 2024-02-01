2e85f9c27284411d8fcbfa9642fdbcb315ceb6f0
Danik <89976419+ByanonymPerson@users.noreply.github.com>
2024-02-01 17:47:31 +0300

    update Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Form.css'
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Form = () => {
+     const [name,setName] = useState('');
+     const [time,setTime] = useState('');
+     const {tg} = useTelegram();
+
+     useEffect(() => {
+       tg.MainButton.setParams({
+        text: 'Отправить данные'
+       })
+     }, [])
+
+     useEffect(() => {
+           if(!time || !name){
+            tg.MainButton.hide();
+           }else {
+            tg.MainButton.show();
+           }
+     },[name,time])
+
+     const onChangeName = (e) => {
+        setName(e.target.value)
+     }
+
+     const onChangeTime = (e) => {
+        setTime(e.target.value)
+     }
+
+   
+
     return(
-        <div>
-        Form
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input
+             className={'input'}
+              type="text" 
+              placeholder={'Имя'}
+              value={name}
+              onChange={onChangeName}
+              />
+              <h3>Введите время в которое вам удобно забрать</h3>
+            <input 
+            className={'input'}
+             type="text" 
+             laceholder={'Время'}
+             value={time}
+             onChange={onChangeTime}
+             />
         </div>
     )
 }
